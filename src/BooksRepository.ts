5c391e498df1438ce0f70a76df84f303edc47523
Elizaveta Smekhova <smekhovaelizaveta@gmail.com>
2025-04-01 10:01:31 +0300

    bugs fixed

--- src/BooksRepository.ts
+++ src/BooksRepository.ts
@@ -1,9 +1,9 @@
 import Book from "./interfaces/Book";
 
 export default abstract class BooksRepository {
-  createBook(book: Book): void;
-  getBook(id: number): Book | null;
-  getBooks(): Book[];
-  updateBook(id: number, updatedBook: Book): void;
-  deleteBook(id: number): void;
+  abstract createBook(book: Book): void;
+  abstract getBook(id: number): Book | null;
+  abstract getBooks(): Book[];
+  abstract updateBook(id: number, updatedBook: Book): void;
+  abstract deleteBook(id: number): void;
 }
